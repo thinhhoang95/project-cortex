b0c91dd2557ed05d9bb1cec9c7730e205c99427c
Thinh Hoang <thinhhoangdinh95@hotmail.com>
2025-09-17 19:24:33 +0200

    Update version number in Header component to 25.09.17.1 (summerbreeze)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -310,7 +310,7 @@ export default function Header() {
                 </button>
                 <div className="mx-4 my-2 glass-menu-divider" />
                 <div className="px-4 pb-3 text-xs glass-menu-muted">
-                  Version 0.24.5 (summerbreeze)
+                  Version 25.09.17.1 (summerbreeze)
                 </div>
               </div>
             )}
